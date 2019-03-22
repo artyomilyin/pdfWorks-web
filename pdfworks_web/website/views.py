0e6abfa9b1887bfa3680430e8cf7f4bc3e04b575
Artyom ILYIN <artyom_ilyin@mail.ru>
2019-03-22 12:56:21 +0300

    created different templates, url routes, working on dropzone

--- pdfworks_web/website/views.py
+++ pdfworks_web/website/views.py
@@ -3,4 +3,23 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request,
-                  'website/homepage.html')
+                  'website/homepage.html',
+                  {'section': 'merge'})
+
+
+def merge(request):
+    return render(request,
+                  'website/merge.html',
+                  {'section': 'merge'})
+
+
+def split(request):
+    return render(request,
+                  'website/split.html',
+                  {'section': 'split'})
+
+
+def offline(request):
+    return render(request,
+                  'website/offline.html',
+                  {'section': 'offline'})
