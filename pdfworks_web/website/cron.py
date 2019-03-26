9ec46c6c22fcf0e12c6a0c51bcaabcc00f45a071
Artyom Ilyin <artyom_ilyin@mail.ru>
2019-03-26 15:37:03 +0300

    migrated to django-crontab

--- pdfworks_web/website/cron.py
+++ pdfworks_web/website/cron.py
@@ -1,17 +1,8 @@
-from django_cron import CronJobBase, Schedule
 from django.utils import timezone
 from .models import RequestFiles
 
 
-class RemoveOldSessions(CronJobBase):
-    RUN_EVERY_MINS = 1
-
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = 'website.remove_old_sessions'
-
-    def do(self):
-        objs = RequestFiles.objects.filter(date_created__lt=timezone.now() - timezone.timedelta(minutes=1))
-        [obj.delete() for obj in objs]
-
-def my_scheduled_job():
-    print("kek")
+def remove_old_sessions():
+    objs = RequestFiles.objects.filter(date_created__lt=timezone.now() - timezone.timedelta(minutes=1))
+    [obj.delete() for obj in objs]
+    print("%s: job finished" % timezone.now())
