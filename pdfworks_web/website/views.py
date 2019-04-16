f6b081f7f60c229d1e582e6bf9f51360db33d4f3
Artyom ILYIN <artyom_ilyin@mail.ru>
2019-04-16 21:39:21 +0300

    comments added to views.py

--- pdfworks_web/website/views.py
+++ pdfworks_web/website/views.py
@@ -14,6 +14,7 @@ from .models import RequestFiles, UploadedFile, Statistic
 
 
 def homepage(request):
+    # just show a homepage
     return render(request,
                   'website/homepage.html',
                   {'section': 'homepage'})
@@ -27,8 +28,12 @@ def merge(request):
         return output
 
     if request.method == "POST":
+        # order_array is filled via javascript on client's side and determines an order for files to be converted/merged
+        # also presence of order_array variable in POST shows that there was a form submission (not AJAX)
         if 'order_array' in request.POST:
+            # get related RequestFiles object (it contains all the uploaded files)
             try:
+                # use csrf token as an identifier
                 request_files_object = RequestFiles.objects.get(csrf_id=request.POST['csrfmiddlewaretoken'])
                 if 'order_array' not in request.POST:
                     raise ObjectDoesNotExist
@@ -37,11 +42,16 @@ def merge(request):
             if request_files_object:
                 print("request object is: %s" % request_files_object)
                 order_array = request.POST['order_array'].split(',')
+
+                # define an order of files and the final list of files
                 preserved = Case(*[When(uuid=uuid, then=pos) for pos, uuid in enumerate(order_array)])
                 files_objects = request_files_object.uploaded_files.filter(uuid__in=order_array).order_by(preserved)
                 files_list = [file.filename.name for file in files_objects]
                 print(files_list)
+
+                # get the name of output file
                 filesys_output_filename = merge_lib_call(files_list, request_files_object.csrf_id)
+                # create an http response with the file an send it to the client
                 with open(filesys_output_filename, 'rb') as file_to_send:
                     response = HttpResponse(file_to_send, 'text/html')
                     if request.POST['output_filename'] != '':
@@ -50,16 +60,20 @@ def merge(request):
                         save_filename = "pdfWorks.org_%s.pdf" % request_files_object.csrf_id[:8]
                     response['Content-Disposition'] = 'attachment; filename="%s"' % save_filename
                     request_files_object.delete(output_filename=filesys_output_filename)
+                    # add a statistics record
                     Statistic.objects.create(output_filename=save_filename,
                                              tool_type='merge',
                                              ip_address=request.META.get('HTTP_X_REAL_IP', 'unknown')).save()
                 return response
         else:
+            # means that it is not a form submission and files are still uploading
+            # get related object or create one if it does not exist yet, use csrf token as an identifier
             try:
                 request_files_object = RequestFiles.objects.get(csrf_id=request.POST['csrfmiddlewaretoken'])
             except ObjectDoesNotExist:
                 request_files_object = RequestFiles(csrf_id=request.POST['csrfmiddlewaretoken'], tool_type='merge')
                 request_files_object.save()
+            # add new uploaded file to RequestFiles object
             uploaded_file = UploadedFile(request_session=request_files_object)
             uploaded_file.filename.save(str(request.FILES['file']), ContentFile(request.FILES['file'].read()))
             uploaded_file.uuid = request.POST['file_uuid']
@@ -73,12 +87,19 @@ def merge(request):
 
 def split(request):
     def split_lib_call(filename, unq_dir):
+        """
+        A function to use pdfworks-lib's split method and to zip pages into an archive.
+        Returns the name of the zip file.
+        """
         output_dir = os.path.join('output', unq_dir)
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
         converter = Converter()
         converter.split_pdf(filename, output_dir)
+
+        # create a zip file
         zipf = zipfile.ZipFile(os.path.join(output_dir, '%s.zip' % unq_dir), 'w', zipfile.ZIP_DEFLATED)
+        # add all the files from split's output to this zip file
         for root, dirs, files in os.walk(os.path.join(settings.BASE_DIR, output_dir)):
             for file in files:
                 if file.endswith('.pdf'):
@@ -91,31 +112,40 @@ def split(request):
 
     if request.method == "POST":
         if request.is_ajax():
+            # means files are still uploading
             file_uuid = request.POST['file_uuid']
             file_csrf = request.POST['csrfmiddlewaretoken']
 
+            # create an instance of RequestFiles model
             unq_file_id = hashlib.sha1(("%s%s" % (file_uuid, file_csrf)).encode('utf-8')).hexdigest()
             request_files_object = RequestFiles(csrf_id=unq_file_id, tool_type='split')
             request_files_object.save()
+            # created a related instance of UploadedFile model
             uploaded_file = UploadedFile(request_session=request_files_object)
             uploaded_file.filename.save(str(request.FILES['file']), ContentFile(request.FILES['file'].read()))
             uploaded_file.save()
             uploaded_filename = uploaded_file.filename.name
+            # split the file and get a zip-archive
             output_filename = split_lib_call(uploaded_filename, request_files_object.csrf_id)
+            # zip is ready to be sent to the client
             print("zip: %s" % output_filename)
         else:
+            # means there was a form submition from javascript and we can serve the file to the client
             file_uuid = request.POST['file_uuid']
             file_csrf = request.POST['csrfmiddlewaretoken']
+            # define which zip to send
             unq_file_id = hashlib.sha1(("%s%s" % (file_uuid, file_csrf)).encode('utf-8')).hexdigest()
             request_files_object = RequestFiles.objects.get(csrf_id=unq_file_id)
             uploaded_file = request_files_object.uploaded_files.first()
             output_filepath = os.path.join('output', os.path.join(unq_file_id, "%s.zip" % unq_file_id))
             filename = os.path.basename(uploaded_file.filename.name)
+            # open zip file and return it to the client
             with open(output_filepath, 'rb') as file_to_send:
                 response = HttpResponse(file_to_send, 'text/html')
                 save_filename = "%s_pdfWorks.org.zip" % filename
                 response['Content-Disposition'] = 'attachment; filename="%s"' % save_filename
                 request_files_object.delete(output_filename=os.path.join('output', request_files_object.csrf_id))
+                # add a statistics record
                 Statistic.objects.create(output_filename=save_filename,
                                          tool_type='split',
                                          ip_address=request.META.get('HTTP_X_REAL_IP', 'unknown')).save()
